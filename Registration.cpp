a13f5246440e500efb5bb96c192b8d10229cf1e4
Jean-Francois Tremblay <jftremblay255@gmail.com>
2016-09-11 11:58:36 -0400

    test.

--- Registration.cpp
+++ Registration.cpp
@@ -5,7 +5,7 @@
 
 namespace tlr
 {
-	
+  
 // Helper functions declaration
 double GetMeanOfVector(const Eigen::Vector4d& coords);
 std::vector<std::set<int>> NCombK(const int n, const int k);
@@ -75,8 +75,8 @@ Registration::computeBestTransform()
 void
 Registration::printFinalReport()
 {
-	//TODO
-	
+  //TODO
+  
 }
 
 void
@@ -97,11 +97,11 @@ Registration::RANSACtransform(PairOfStemGroups& pair)
       {
         if(!this->stemDistanceGreaterThanTol(sourceCopy.getStems()[i],
                                              this->target.getStems()[j])
-	         &&
-	         !this->stemAlreadyInGroup(this->target.getStems()[j],
+           &&
+           !this->stemAlreadyInGroup(this->target.getStems()[j],
                                      pair.getTargetGroup())
-	         &&
-	         !this->relDiamErrorGreaterThanTol(this->target.getStems()[j],
+           &&
+           !this->relDiamErrorGreaterThanTol(this->target.getStems()[j],
                                              this->source.getStems()[i]))
         {
           // We add the stem who was not transformed
@@ -133,7 +133,7 @@ bool
 Registration::stemDistanceGreaterThanTol(const Stem& stem1, const Stem& stem2) const
 {
   Eigen::Vector4d stemError = stem1.getCoords()
-		                          - stem2.getCoords();
+                              - stem2.getCoords();
   return stemError.norm() > this->RANSACtol;
 }
 
@@ -142,7 +142,7 @@ bool
 Registration::relDiamErrorGreaterThanTol(const Stem& stem1, const Stem& stem2) const
 {
   return fabs(stem1.getRadius() - stem2.getRadius()) /
-    	   ((stem1.getRadius() + stem2.getRadius())/2) < this->diamErrorTol;
+         ((stem1.getRadius() + stem2.getRadius())/2) < this->diamErrorTol;
 }
 
 Registration::~Registration()
@@ -185,118 +185,118 @@ Registration::removeLonelyStems()
       this->source.removeStem(indicesToRemove[j]);
     }
    }
-	// Repeat for the target map
-	indicesToRemove = {};
-	i = 0;
-	for (auto& itTarget : this->target.getStems())
-	{
-		toBeRemoved = true;
-		for (auto& itSource : this->source.getStems())
-		{
-			if (!this->relDiamErrorGreaterThanTol(
-				      itSource, itTarget
-				 ))
-			{
-				toBeRemoved = false; 
-			}
-		}
-		if (toBeRemoved)
-		{
-			indicesToRemove.push_back(i);
-			++nRemoved;
-		}
-		++i;
-	}
-	if (indicesToRemove.size() > 0)
-	{
-		// Remove the stems backward so the indices are not all shuffled up
-		for (size_t j = indicesToRemove.size() - 1; j > 0; --j)
-		{
-			this->target.removeStem(indicesToRemove[j]);
-		}
-	}
-	return nRemoved;
+  // Repeat for the target map
+  indicesToRemove = {};
+  i = 0;
+  for (auto& itTarget : this->target.getStems())
+  {
+    toBeRemoved = true;
+    for (auto& itSource : this->source.getStems())
+    {
+      if (!this->relDiamErrorGreaterThanTol(
+              itSource, itTarget
+         ))
+      {
+        toBeRemoved = false; 
+      }
+    }
+    if (toBeRemoved)
+    {
+      indicesToRemove.push_back(i);
+      ++nRemoved;
+    }
+    ++i;
+  }
+  if (indicesToRemove.size() > 0)
+  {
+    // Remove the stems backward so the indices are not all shuffled up
+    for (size_t j = indicesToRemove.size() - 1; j > 0; --j)
+    {
+      this->target.removeStem(indicesToRemove[j]);
+    }
+  }
+  return nRemoved;
 }
 
 long long
 Factorial(int n)
 {
-	long long result = 1;
-	while (n>1) {
-		result *= n--;
-	}
-	return result;
+  long long result = 1;
+  while (n>1) {
+    result *= n--;
+  }
+  return result;
 }
 
 // Generates all 3 from K combination
 std::vector<std::set<int>>
 ThreeCombK(const unsigned int k)
 {
-	std::vector<std::set<int>> result;
-	std::set<int> comb;
-		
-	for (unsigned int i = 1; i < k - 1; ++i)
-	{
-		for (unsigned int j = i; j < k; ++j)
-		{
-			for (unsigned int s = j; s <= k; ++s)
-			{
-				comb.insert(i);
-				comb.insert(j);
-				comb.insert(s);
-				// Only add combinaison if the numbers are unique
-				if (comb.size() == 3)
-					result.push_back(comb);
-				comb = std::set<int>();
-			}
-		}
-	}
+  std::vector<std::set<int>> result;
+  std::set<int> comb;
+    
+  for (unsigned int i = 1; i < k - 1; ++i)
+  {
+    for (unsigned int j = i; j < k; ++j)
+    {
+      for (unsigned int s = j; s <= k; ++s)
+      {
+        comb.insert(i);
+        comb.insert(j);
+        comb.insert(s);
+        // Only add combinaison if the numbers are unique
+        if (comb.size() == 3)
+          result.push_back(comb);
+        comb = std::set<int>();
+      }
+    }
+  }
 
-	return result;
+  return result;
 }
 
 void
 Registration::generateEigenValues(StemTriplet& triplet)
 {
-	// Can be coded much cleaner. Needs to be revisited.
-	Eigen::Matrix3d covarianceMatrix;
-	for (int i = 0; i < 3; ++i)
-	{
-		for (int j = 0; j < 3; ++j)
-		{
-			covarianceMatrix(i, j) =
-				 ((std::get<0>(triplet))[i]->getCoords()[0] - GetMeanOfVector((std::get<0>(triplet))[i]->getCoords()))
-				*((std::get<0>(triplet))[j]->getCoords()[0] - GetMeanOfVector((std::get<0>(triplet))[j]->getCoords()))
-				+((std::get<0>(triplet))[i]->getCoords()[1] - GetMeanOfVector((std::get<0>(triplet))[i]->getCoords()))
-				*((std::get<0>(triplet))[j]->getCoords()[1] - GetMeanOfVector((std::get<0>(triplet))[j]->getCoords()))
-				+((std::get<0>(triplet))[i]->getCoords()[2] - GetMeanOfVector((std::get<0>(triplet))[i]->getCoords()))
-				*((std::get<0>(triplet))[j]->getCoords()[2] - GetMeanOfVector((std::get<0>(triplet))[j]->getCoords()));
-		}
-	}
-	Eigen::Matrix3d::EigenvaluesReturnType eigenvalues = covarianceMatrix.eigenvalues();
-	std::get<1>(triplet).push_back(eigenvalues(0));
-	std::get<1>(triplet).push_back(eigenvalues(1));
-	std::get<1>(triplet).push_back(eigenvalues(2));
+  // Can be coded much cleaner. Needs to be revisited.
+  Eigen::Matrix3d covarianceMatrix;
+  for (int i = 0; i < 3; ++i)
+  {
+    for (int j = 0; j < 3; ++j)
+    {
+      covarianceMatrix(i, j) =
+         ((std::get<0>(triplet))[i]->getCoords()[0] - GetMeanOfVector((std::get<0>(triplet))[i]->getCoords()))
+        *((std::get<0>(triplet))[j]->getCoords()[0] - GetMeanOfVector((std::get<0>(triplet))[j]->getCoords()))
+        +((std::get<0>(triplet))[i]->getCoords()[1] - GetMeanOfVector((std::get<0>(triplet))[i]->getCoords()))
+        *((std::get<0>(triplet))[j]->getCoords()[1] - GetMeanOfVector((std::get<0>(triplet))[j]->getCoords()))
+        +((std::get<0>(triplet))[i]->getCoords()[2] - GetMeanOfVector((std::get<0>(triplet))[i]->getCoords()))
+        *((std::get<0>(triplet))[j]->getCoords()[2] - GetMeanOfVector((std::get<0>(triplet))[j]->getCoords()));
+    }
+  }
+  Eigen::Matrix3d::EigenvaluesReturnType eigenvalues = covarianceMatrix.eigenvalues();
+  std::get<1>(triplet).push_back(eigenvalues(0));
+  std::get<1>(triplet).push_back(eigenvalues(1));
+  std::get<1>(triplet).push_back(eigenvalues(2));
 }
 
 void
 Registration::generateAllEigenValues()
 {
-	for (auto& it : this->threePermSource)
-	{
-		this->generateEigenValues(it);
-	}
-	for (auto& it : this->threePermTarget)
-	{
-		this->generateEigenValues(it);
-	}
+  for (auto& it : this->threePermSource)
+  {
+    this->generateEigenValues(it);
+  }
+  for (auto& it : this->threePermTarget)
+  {
+    this->generateEigenValues(it);
+  }
 }
 
 // This is useful for computing the covariance matrix.
 double
 GetMeanOfVector(const Eigen::Vector4d& coords)
 {
-	return (coords[0] + coords[1] + coords[2]) / 3;
+  return (coords[0] + coords[1] + coords[2]) / 3;
 }
 
 /* This function populate the stem triplets from both the target scan and the source scan.
@@ -304,62 +304,62 @@ GetMeanOfVector(const Eigen::Vector4d& coords)
 void
 Registration::generateTriplets(StemMap& stemMap, std::vector<StemTriplet>& threePerm)
 {
-	std::vector<std::set<int>> threePermN = ThreeCombK(stemMap.getStems().size());
-	StemTriplet tempTriplet = StemTriplet();
-	
-	for (auto it : threePermN)
-	{
-		for (auto jt : it)
-			std::get<0>(tempTriplet).push_back(&stemMap.getStems()[jt - 1]);
-		threePerm.push_back(tempTriplet);
-		tempTriplet = StemTriplet();
-	}
+  std::vector<std::set<int>> threePermN = ThreeCombK(stemMap.getStems().size());
+  StemTriplet tempTriplet = StemTriplet();
+  
+  for (auto it : threePermN)
+  {
+    for (auto jt : it)
+      std::get<0>(tempTriplet).push_back(&stemMap.getStems()[jt - 1]);
+    threePerm.push_back(tempTriplet);
+    tempTriplet = StemTriplet();
+  }
 }
 
 // Population the pairOfStemsTriplets attributes with all possible pairs. Cleanup is done later.
 void
 Registration::generatePairs()
 {
-	#pragma omp parallel for
-	for (size_t i = 0; i < this->threePermSource.size(); ++i)
-	{
-		for (size_t j = 0; j < this->threePermTarget.size(); ++j)
-		{
-			PairOfStemGroups tempPair(this->threePermTarget[j],
+  #pragma omp parallel for
+  for (size_t i = 0; i < this->threePermSource.size(); ++i)
+  {
+    for (size_t j = 0; j < this->threePermTarget.size(); ++j)
+    {
+      PairOfStemGroups tempPair(this->threePermTarget[j],
                                 this->threePermSource[i]);
 
-			if(!this->diametersNotCorresponding(tempPair)
-			   && this->pairPositionsAreCorresponding(tempPair))
-			{	
-				#pragma omp critical
-				{
-					this->pairsOfStemTriplets.push_back(tempPair);
-				}
-			}
-		}
-	}
+      if(!this->diametersNotCorresponding(tempPair)
+         && this->pairPositionsAreCorresponding(tempPair))
+      {	
+        #pragma omp critical
+        {
+          this->pairsOfStemTriplets.push_back(tempPair);
+        }
+      }
+    }
+  }
 }
 
 void
 Registration::removeHighlyColinearTriplets(std::vector<StemTriplet>& triplets)
 {
-	triplets.erase(
-		std::remove_if(
-			triplets.begin(),
-			triplets.end(),
-			ColinearityGreaterThanTol),
-		triplets.end());
+  triplets.erase(
+    std::remove_if(
+      triplets.begin(),
+      triplets.end(),
+      ColinearityGreaterThanTol),
+    triplets.end());
 }
 
 // This removes of non-matching (diameter-wise) pair of triplets.
 bool
 Registration::diametersNotCorresponding(PairOfStemGroups& pair)
 {
-	for(auto& it : pair.getRadiusSimilarity())
-	{
-		if(this->diamErrorGreaterThanTol(it)) return true;
-	}
-	return false;
+  for(auto& it : pair.getRadiusSimilarity())
+  {
+    if(this->diamErrorGreaterThanTol(it)) return true;
+  }
+  return false;
 }
 
 /* This asserts wether the stems are positioned in the same way relative
@@ -371,19 +371,19 @@ Registration::diametersNotCorresponding(PairOfStemGroups& pair)
 bool
 Registration::pairPositionsAreCorresponding(PairOfStemGroups& pair)
 {
-	const std::vector<double> verticeDiffs = pair.getVerticeDifference();
-	for(double diff : verticeDiffs)
-	{
-		if(diff > 2*this->RANSACtol) return false;
-	}
-	return true;
+  const std::vector<double> verticeDiffs = pair.getVerticeDifference();
+  for(double diff : verticeDiffs)
+  {
+    if(diff > 2*this->RANSACtol) return false;
+  }
+  return true;
 }
 
 // This is used for the removal of non-matching pair of triplets (DiametersNotCorresponding).
 bool
 Registration::diamErrorGreaterThanTol(double error)
 {
-	return error > this->diamErrorTol;
+  return error > this->diamErrorTol;
 }
 
 /* This is used to determine if the triplet of stem is too linear, which makes
@@ -391,9 +391,9 @@ Registration::diamErrorGreaterThanTol(double error)
 bool
 ColinearityGreaterThanTol(StemTriplet& triplet)
 {
-	return std::get<1>(triplet)[0].real() /
-		     (std::get<1>(triplet)[0].real() + std::get<1>(triplet)[1].real())
-		     > LINEARITY_TOL;
+  return std::get<1>(triplet)[0].real() /
+         (std::get<1>(triplet)[0].real() + std::get<1>(triplet)[1].real())
+         > LINEARITY_TOL;
 }
 
 } // namespace tlr
