655b079844264798b8a6aa44cfb5191b1bd5e6af
Jean-Francois Tremblay <jftremblay255@gmail.com>
2018-02-16 04:12:31 -0500

    Fix pour le perf graph

--- python_utils/error_box_plot.py
+++ python_utils/error_box_plot.py
@@ -9,6 +9,24 @@ import sys
 import os
 from matplotlib.patches import Polygon
 
+def adjustFigAspect(fig,aspect=1):
+    '''
+    Adjust the subplot parameters so that the figure has the correct
+    aspect ratio.
+    '''
+    xsize,ysize = fig.get_size_inches()
+    minsize = min(xsize,ysize)
+    xlim = .4*minsize/xsize
+    ylim = .4*minsize/ysize
+    if aspect < 1:
+        xlim *= aspect
+    else:
+        ylim /= aspect
+    fig.subplots_adjust(left=.5-xlim,
+                        right=.5+xlim,
+                        bottom=.5-ylim,
+                        top=.5+ylim)
+
 def getRegError(path):
     print("On traite le fichier {}".format(path))
     file = open(path, 'r')
@@ -29,7 +47,7 @@ def getRegError(path):
         i += 1
     return np.linalg.norm(R, ord=2), np.linalg.norm(t, ord=2)
 
-rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
+rc('font', **{'family': 'Calibri'})
 
 # Generate some data from five different probability distributions,
 # each with different characteristics. We want to play with how an IID
@@ -37,14 +55,14 @@ rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
 # properties of the original sample, and a boxplot is one visual tool
 # to make this assessment
 numSites = 4
-sites = [('Sequoia', 'resultSequoia'), 
+sites = [('redwood plantation', 'resultSequoia'), 
          ('Valcartier 1', 'result1'),
          ('Valcartier 2', 'result5'),
-         ('Savanne', 'result_savanne')]
-siteNames = ['Sequoia',
+         ('savanna', 'result_savanne')]
+siteNames = ['redwood plantation',
              'Valcartier 1',
              'Valcartier 2',
-             'Savanne']
+             'savanna']
 directory = sys.argv[1]
 
 dataTrans = []
@@ -68,10 +86,13 @@ fig, ax1 = plt.subplots(figsize=(10, 6))
 fig.canvas.set_window_title('A Boxplot Example')
 plt.subplots_adjust(left=0.075, right=0.95, top=0.9, bottom=0.25)
 
-bp = plt.boxplot(dataTrans, notch=0, sym='+', vert=1, whis=1.5)
+# Set data here!!!
+dataset = dataRot
+
+bp = plt.boxplot(dataset, notch=0, sym='+', vert=1, whis=1.5)
 plt.setp(bp['boxes'], color='black')
 plt.setp(bp['whiskers'], color='black')
-plt.setp(bp['fliers'], color='red', marker='+')
+plt.setp(bp['fliers'], color='red', marker='+', linewidth=16)
 
 # Add a horizontal grid to the plot, but make it very light in color
 # so we can use it for reading data values but not be distracting
@@ -80,12 +101,13 @@ ax1.yaxis.grid(True, linestyle='-', which='major', color='lightgrey',
 
 # Hide these grid behind plot objects
 ax1.set_axisbelow(True)
-ax1.set_title('Translation error for each test site')
-ax1.set_xlabel('Site')
-ax1.set_ylabel(r'$||\mathbf{t}_{algorithm} - \mathbf{t}_{targets}||$ (meters)')
+ax1.set_title('Rotation error for each test site', fontsize=20)
+ax1.set_xlabel('Site', fontsize=16)
+ax1.set_ylabel(r'$||R_{algorithm} - R_{targets}||_2$', fontsize=16)
+
 
 # Now fill the boxes with desired colors
-boxColors = ['darkkhaki', 'royalblue']
+boxColors = ['lightgrey', 'lightgrey']
 numBoxes = numSites
 medians = list(range(numBoxes))
 for i in range(numBoxes):
@@ -102,6 +124,9 @@ for i in range(numBoxes):
     ax1.add_patch(boxPolygon)
     # Now draw the median lines back over what we just filled in
     med = bp['medians'][i]
+    print("Medianes:")
+    for line in bp['medians']:
+        print(line.get_ydata())
     medianX = []
     medianY = []
     for j in range(2):
@@ -111,7 +136,7 @@ for i in range(numBoxes):
         medians[i] = medianY[0]
     # Finally, overplot the sample averages, with horizontal alignment
     # in the center of each box
-    plt.plot([np.average(med.get_xdata())], [np.average(dataTrans[i])],
+    plt.plot([np.average(med.get_xdata())], [np.average(dataset[i])],
              color='w', marker='*', markeredgecolor='k')
 
 # Set the axes ranges and axes labels
@@ -120,8 +145,10 @@ top = 40
 bottom = -5
 ax1.set_ylim(auto=True)
 xtickNames = plt.setp(ax1, xticklabels=siteNames)
-plt.setp(xtickNames, rotation=45, fontsize=8)
-
+plt.setp(xtickNames, rotation=45, fontsize=12)
+x0,x1 = ax1.get_xlim()
+y0,y1 = ax1.get_ylim()
+ax1.set_aspect(0.85*abs(x1-x0)/abs(y1-y0))
 # Due to the Y-axis scale being different across samples, it can be
 # hard to compare differences in medians across the samples. Add upper
 # X-axis tick labels with the sample medians to aid in comparison
@@ -134,5 +161,6 @@ for tick, label in zip(range(numBoxes), ax1.get_xticklabels()):
     ax1.text(pos[tick], top - (top*0.05), upperLabels[tick],
              horizontalalignment='center', size='small', weight=weights[k],
              color=boxColors[k])
-
+print("Medianes:")
+print(medians)
 plt.show()
--- python_utils/perf_graph.py
+++ python_utils/perf_graph.py
@@ -9,15 +9,15 @@ if __name__=="__main__":
     rc('xtick', labelsize=15)
     rc('ytick', labelsize=15)
 
-    x = range(1,9)
-    data_new = [63,74,68,48,40,36,32,30]
-    data_old = [287,288,272,289,255,250,244,258]
+    x = range(1,5)
+    data_new = [34,19,14,12]
+    data_old = [279,260,259,265]
 
     plt.plot(x, data_new, color="blue") 
     plt.plot(x, data_old, color="green")
     plt.xlabel("Number of threads", fontsize=18)
     plt.ylabel("Execution time (s)", fontsize=18)
-    plt.title("Comparison of execution times between Kelbe \net al.'s algorithm and ours. 40 stems in each scan.", fontsize=20)
+    plt.title("Comparison of execution times between Kelbe \net al.'s algorithm and ours.", fontsize=20)
     old_patch = mpatches.Patch(color='green', label="Kelbe et al. imitation")
     new_patch = mpatches.Patch(color='blue', label="Our proposed method")
     plt.legend(handles=[new_patch,old_patch])
